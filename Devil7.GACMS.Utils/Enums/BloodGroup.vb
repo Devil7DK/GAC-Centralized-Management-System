48651a884772868844c04a2368b9a9ff6a035e97
Devil7DK <dineshthangavel47@gmail.com>
2018-08-30 21:24:00 +0530

    Utils: Add More Enums

---
+++ Devil7.GACMS.Utils/Enums/BloodGroup.vb
@@ -0,0 +1,35 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+' 
+'     http://www.apache.org/licenses/LICENSE-2.0
+' 
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Namespace Enums
+
+    Public Enum BloodGroup As Integer
+        AB_Negative = -4
+        B_Negative = -3
+        A_Negative = -2
+        O_Negative = -1
+        Unknown = 0
+        O_Positive = 1
+        A_Positive = 2
+        B_Positive = 3
+        AB_Positive = 4
+    End Enum
+
+End Namespace
---
+++ Devil7.GACMS.Utils/Enums/Religion.vb
@@ -0,0 +1,37 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+' 
+'     http://www.apache.org/licenses/LICENSE-2.0
+' 
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Namespace Enums
+
+    Public Enum Religion As Integer
+        Unknown = 0
+        Hinduism = 1
+        Islam = 2
+        Christianity
+        Buddhism = 4
+        Jainism = 5
+        Sikhism = 6
+        Zoroastrian = 7
+        GuruNanakDev = 8
+        LordMahavira = 9
+        AdiShankaracharya = 10
+    End Enum
+
+End Namespace
