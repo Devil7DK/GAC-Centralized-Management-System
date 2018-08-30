1a7e4c35934763817cb9e619a10ac168c9d45f02
Devil7DK <dineshthangavel47@gmail.com>
2018-08-30 16:34:11 +0530

    WAL: Add Interface for 'Staffs'

---
+++ Devil7.GACMS.WAL/IStaffs.vb
@@ -0,0 +1,28 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+' 
+'     http://www.apache.org/licenses/LICENSE-2.0
+' 
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Imports Devil7.GACMS.Utils.Objects
+
+Public Interface IStaffs
+
+    <OperationContract>
+    Function GetStaff(ByVal Username As String, ByVal Password As String) As Staff
+
+End Interface
