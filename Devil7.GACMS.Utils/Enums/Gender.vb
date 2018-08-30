3122fc6934804fa7ec0ce95998617eb283f7f818
Devil7DK <dineshthangavel47@gmail.com>
2018-08-30 16:21:46 +0530

    Utils: Add 'Gender' Enum

---
+++ Devil7.GACMS.Utils/Enums/Gender.vb
@@ -0,0 +1,29 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+' 
+'     http://www.apache.org/licenses/LICENSE-2.0
+' 
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Namespace Enums
+
+    Public Enum Gender As Integer
+        Male = 0
+        Female = 1
+        TransGender = 2
+    End Enum
+
+End Namespace
