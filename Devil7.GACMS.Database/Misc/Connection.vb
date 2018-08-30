a99d29bb48a911ecc2904fbf2ec5e6f4a935f614
Devil7DK <dineshthangavel47@gmail.com>
2018-08-30 17:13:40 +0530

    Database: Add Initial SQL Functions

---
+++ Devil7.GACMS.Database/Misc/Connection.vb
@@ -0,0 +1,44 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+' 
+'     http://www.apache.org/licenses/LICENSE-2.0
+' 
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Imports Devil7.GACMS.Utils.Modules
+Imports System.Data.SqlClient
+
+Public Module Connection
+
+    Dim ServerSettings As Settings
+    Dim Connection As SqlConnection
+
+    Public Function GetConnection() As SqlConnection
+        If Connection Is Nothing Then
+            Connection = CreateConnection()
+        End If
+        Return Connection
+    End Function
+
+    Private Function CreateConnection() As SqlConnection
+        If ServerSettings Is Nothing Then
+            ServerSettings = Settings.GetSettings
+        End If
+        Return New SqlConnection(String.Format("Server={0};Database={1};User Id={2};Password={3};Pooling={5}",
+                                               ServerSettings.ServerName, ServerSettings.DatabaseName, ServerSettings.UserName, Encryption.Decrypt(ServerSettings.Password), ServerSettings.Pooling))
+    End Function
+
+End Module
---
+++ Devil7.GACMS.Database/Misc/Settings.vb
@@ -0,0 +1,45 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+' 
+'     http://www.apache.org/licenses/LICENSE-2.0
+' 
+' Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Imports Devil7.GACMS.Utils
+Imports System.Reflection
+
+Public Class Settings
+
+    Private Shared ConfigFilePath As String = IO.Path.Combine(My.Computer.FileSystem.GetFileInfo((New System.Uri(Assembly.GetExecutingAssembly().CodeBase)).AbsolutePath).Directory.FullName, "DatabseServer.config")
+
+    Public Shared Function GetSettings() As Settings
+        On Error Resume Next
+        Dim R As New Settings
+        If My.Computer.FileSystem.FileExists(ConfigFilePath) Then
+            R = Objects.Serializer.FromFile(Of Settings)(ConfigFilePath)
+        Else
+            Objects.Serializer.ToFile(New Settings, ConfigFilePath)
+        End If
+        Return R
+    End Function
+
+    Property DatabaseName As String = "GACMS"
+    Property ServerName As String = "LOCALHOST"
+    Property Password As String = "admin"
+    Property UserName As String = "admin"
+    Property Pooling As Boolean = False
+
+End Class
